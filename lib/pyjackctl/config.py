6cfafae9d2b55690aae5b6183dcce2fa43abb3ab
nedko <nedko@bfe161da-02ef-4cea-8c43-ae261ea21ac6>
2008-07-12 13:50:26 +0000

    Remove not working workaround for ubuntu brain dead ideas.
    Proper fix will be part of ubuntu package.
    
    
    git-svn-id: svn+ssh://svn.marcochapeau.org/laditools/trunk@112 bfe161da-02ef-4cea-8c43-ae261ea21ac6

--- lib/pyjackctl/config.py
+++ lib/pyjackctl/config.py
@@ -14,12 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Dirty workaround for some Ubuntu crap
-try:
-    sys.path.append('/usr/lib/python%s/site-packages/oldxml' % sys.version[:3])
-except:
-    print "Ubuntu workaround not needed it seems..."
-
 import xml.dom
 from xml.dom.minidom import parse, getDOMImplementation
 from xml.dom.ext import PrettyPrint
