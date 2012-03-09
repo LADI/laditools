d0dcc93284409ed32d830efd41ed363704891a3c
Alessio Treglia <alessio@debian.org>
2012-03-09 07:56:51 +0100

    Rename a2j_menu -> a2jmenu

--- laditools/gtk/__init__.py
+++ laditools/gtk/__init__.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from a2j_menu import A2jMenu
+from a2jmenu import A2jMenu
 from ladimenu import LadiManager
 from utils import find_data_file
 
