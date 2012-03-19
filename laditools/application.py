87ff4a1e0da75827278723ce98194bc93e8866a6
Alessio Treglia <alessio@debian.org>
2012-03-19 01:34:57 +0100

    New virtual class for LADI applications.

---
+++ laditools/application.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# LADITools - Linux Audio Desktop Integration Tools
+# ladi-system-tray - System tray integration for LADI
+# Copyright (C) 2011-2012 Alessio Treglia <quadrispro@ubuntu.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from abc import *
+
+class __AppMeta:
+
+    __metaclass__ = ABCMeta
+
+    @abstractproperty
+    def appname(self): pass
+
+    @abstractproperty
+    def appname_long(self): pass
+
+    @abstractproperty
+    def appid(self): pass
+
+    @abstractmethod
+    def run(self): pass
+
+class LadiApp(__AppMeta):
+
+    @property
+    def appname(self): return self._appname
+
+    @property
+    def appid(self): return self._appid
